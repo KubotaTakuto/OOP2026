edb29e427b64b9fc1b5dcd89c9d15c916b964dc9
久保田 拓斗 <oibc37056@ojs.ac.jp>
2026-04-20 11:43:16 +0900

    P30コード2.3まで入力

--- Chapter00/sample0416/Program.cs
+++ Chapter00/sample0416/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             int i = 0;
-            while (i < 10)
+            while (i < 33550336)
             {
                 Console.WriteLine("Hello, World!");
                 i++;
