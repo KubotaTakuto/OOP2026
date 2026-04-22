2cacdc0b55dabfe159f457f10ca86233678bc32c
久保田 拓斗 <oibc37056@ojs.ac.jp>
2026-04-22 12:00:28 +0900

    クラスとして分離

--- Chapter02/距離換算プログラム/DistanceConverter/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -11,13 +11,13 @@ namespace DistanceConverter
                 && int.TryParse(args[2], out var stop)
                 && start <= stop)
             {
-                if (args.Length >= 1 && args[0] == "-tom")
-                {
-                    PrintFeetToMeterList(start, stop);
-                }
-                else if (args.Length >= 1 && args[0] == "-tof")
+                if (args.Length >= 1 && args[0] == "-tof")
                 {
                     PrintMeterToFeetList(start, stop);
+                }
+                else if (args.Length >= 1 && args[0] == "-tom")
+                {
+                    PrintFeetToMeterList(start, stop);
                 }
                 else
                 {
@@ -30,36 +30,26 @@ namespace DistanceConverter
             }
         }
 
-        static void PrintFeetToMeterList(int start, int stop)
-        {
-            //フィートからメートルへの対応表を出力
-            for (int feet = start; feet <= stop; feet++)
-            {
-                double meter = FeetToMeter(feet);
-                Console.WriteLine($"{feet}ft = {meter:0.0000}m");
-            }
-        }
-
         static void PrintMeterToFeetList(int start, int stop)
         {
             //メートルからフィートへの対応表を出力
             for (int meter = start; meter <= stop; meter++)
             {
-                double feet = MeterToFeet(meter);
+                FeetConverter converter = new FeetConverter();
+                double feet = converter.FromMeter(meter);
                 Console.WriteLine($"{meter}m = {feet:0.0000}ft");
             }
         }
 
-        //フィートからメートルを求める
-        static double FeetToMeter(int feet)
+        static void PrintFeetToMeterList(int start, int stop)
         {
-            return feet * 0.3048;
-        }
-
-        //メートルからフィートを求める
-        static double MeterToFeet(int meter)
-        {
-            return meter / 0.3048;
+            //フィートからメートルへの対応表を出力
+            for (int feet = start; feet <= stop; feet++)
+            {
+                FeetConverter converter = new FeetConverter();
+                double meter = converter.ToMeter(feet);
+                Console.WriteLine($"{feet}ft = {meter:0.0000}m");
+            }
         }
     }
 }
