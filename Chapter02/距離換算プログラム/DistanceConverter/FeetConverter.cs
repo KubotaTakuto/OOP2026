4c57e687dba8380d00d7799a9b902c2c06d5a89a
久保田 拓斗 <oibc37056@ojs.ac.jp>
2026-04-23 10:29:45 +0900

    constからstatic readonlyに変換

--- Chapter02/距離換算プログラム/DistanceConverter/FeetConverter.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/FeetConverter.cs
@@ -4,27 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DistanceConverter
-{
-    public static class FeetConverter
-    {
+namespace DistanceConverter {
+    public static class FeetConverter {
+        private static readonly double ratio = 0.3048;    //定数
+
         //メートルからフィートを求める
-        public static double FromMeter(int meter)
-        {
-            return meter / 0.3048;
+        public static double FromMeter(int meter) {
+            return meter / ratio;
         }
 
         //フィートからメートルを求める
-        public static double ToMeter(int feet)
-        {
-            return feet * 0.3048;
+        public static double ToMeter(int feet) {
+            return feet * ratio;
         }
-
-
-
-
-
-
-
     }
 }
--- Chapter02/距離換算プログラム/DistanceConverter/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -1,50 +1,35 @@
 ﻿
 
-namespace DistanceConverter
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+namespace DistanceConverter {
+    internal class Program {
+        static void Main(string[] args) {
             if (args.Length == 3
                 && int.TryParse(args[1], out var start)
                 && int.TryParse(args[2], out var stop)
-                && start <= stop)
-            {
-                if (args.Length >= 1 && args[0] == "-tof")
-                {
+                && start <= stop) {
+                if (args.Length >= 1 && args[0] == "-tof") {
                     PrintMeterToFeetList(start, stop);
-                }
-                else if (args.Length >= 1 && args[0] == "-tom")
-                {
+                } else if (args.Length >= 1 && args[0] == "-tom") {
                     PrintFeetToMeterList(start, stop);
-                }
-                else
-                {
+                } else {
                     Console.WriteLine("引数エラー");
                 }
-            }
-            else
-            {
+            } else {
                 Console.WriteLine("引数エラー");
             }
         }
 
-        static void PrintMeterToFeetList(int start, int stop)
-        {
+        static void PrintMeterToFeetList(int start, int stop) {
             //メートルからフィートへの対応表を出力
-            for (int meter = start; meter <= stop; meter++)
-            {
+            for (int meter = start; meter <= stop; meter++) {
                 double feet = FeetConverter.FromMeter(meter);
                 Console.WriteLine($"{meter}m = {feet:0.0000}ft");
             }
         }
 
-        static void PrintFeetToMeterList(int start, int stop)
-        {
+        static void PrintFeetToMeterList(int start, int stop) {
             //フィートからメートルへの対応表を出力
-            for (int feet = start; feet <= stop; feet++)
-            {
+            for (int feet = start; feet <= stop; feet++) {
                 double meter = FeetConverter.ToMeter(feet);
                 Console.WriteLine($"{feet}ft = {meter:0.0000}m");
             }
