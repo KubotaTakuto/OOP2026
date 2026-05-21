83c413798e9d7f3b0edcc18901909e4d685f6761
久保田 拓斗 <oibc37056@ojs.ac.jp>
2026-05-21 10:32:37 +0900

    問題3.1.3完成

--- Chapter03/演習問題/Execrcise01/Program.cs
+++ Chapter03/演習問題/Execrcise01/Program.cs
@@ -32,11 +32,15 @@ namespace Execrcise01 {
 
         private static void Exercise2(List<int> numbers) {
             Console.WriteLine("---3.1.2---");
-            numbers.ForEach(s =>Console.WriteLine(s/2.0));
+            numbers.ForEach(n =>Console.WriteLine(n/2.0));
         }
 
         private static void Exercise3(List<int> numbers) {
-
+            Console.WriteLine("---3.1.3---");
+            var Exists = numbers.Where(s => s >= 50);
+            foreach (var num in Exists) {
+                Console.WriteLine(num);
+            }
         }
 
         private static void Exercise4(List<int> numbers) {
