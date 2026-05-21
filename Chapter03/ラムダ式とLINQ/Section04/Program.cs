333887bbc8461f0fbdab09dd3d98206ff8e10ddd
久保田 拓斗 <oibc37056@ojs.ac.jp>
2026-05-21 09:49:27 +0900

    問題3.1.1完成

--- Chapter03/ラムダ式とLINQ/Section04/Program.cs
+++ Chapter03/ラムダ式とLINQ/Section04/Program.cs
@@ -1,7 +1,28 @@
 ﻿namespace Section04 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            var cities = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
+
+            var query = cities.Where(s => s.Length <= 5).ToList();
+            foreach (var item in query) {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("------");
+
+            cities[0] = "Osaka";
+            foreach (var item in query) {
+                Console.WriteLine(item);
+            }
         }
     }
 }
