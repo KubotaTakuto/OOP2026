cbdb17a884a741fdc6872ad8060c90731c75cc2a
久保田 拓斗 <oibc37056@ojs.ac.jp>
2026-05-21 11:22:28 +0900

    問題3.2.2完成

--- Chapter03/演習問題/Exercise02/Program.cs
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -40,7 +40,8 @@ namespace Exercise02 {
 
         private static void Exercise2_2(List<string> cities) {
             //できたらGitのコメント「問題3.2.2完成」
-
+            var Exists = cities.Count(s => s.Contains('o'));
+            Console.WriteLine(Exists);
         }
 
         private static void Exercise2_3(List<string> cities) {
