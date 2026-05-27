2e398e9add38d5c9dac85f16b9bee02c4dcd2a1b
久保田 拓斗 <oibc37056@ojs.ac.jp>
2026-05-27 12:05:28 +0900

    問題4.2.1完成

---
+++ Chapter04/演習問題/Exercise02/Exercise02/Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(" --- 4.2.1 ---");
+            Exercise1();
+            Console.WriteLine("\n --- 4.2.2 ---");
+            Exercise2();
+            Console.WriteLine("\n --- 4.2.3 ---");
+            Exercise3();
+        }
+
+        //P89～P90を参考に
+        private static void Exercise1() {
+            //if-else文を使用（コード4.9）
+            var number = Console.ReadLine();
+            int num = int.Parse(number);
+            if (num >= 500) {
+                Console.WriteLine(num);
+            } else if (num >= 100) {
+                Console.WriteLine(num * 3);
+            } else if (num >= 0) {
+                Console.WriteLine(num * 2);
+            } else {
+                Console.WriteLine(num);
+            }
+        }
+
+        private static void Exercise2() {
+            //switch文を使用（コード4.10）
+
+        }
+
+        private static void Exercise3() {
+            //switch式を使用（コード4.11）
+
+
+        }
+    }
+}
