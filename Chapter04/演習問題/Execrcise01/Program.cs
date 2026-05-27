9d737ad1764556d383fd8a80be4b439684c1d981
久保田 拓斗 <oibc37056@ojs.ac.jp>
2026-05-27 11:53:40 +0900

    問題4.1.3完成

--- Chapter04/演習問題/Execrcise01/Program.cs
+++ Chapter04/演習問題/Execrcise01/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace Execrcise01 {
+﻿
+namespace Execrcise01 {
     internal class Program {
         static void Main(string[] args) {
             List<string> langs = [
@@ -42,21 +43,21 @@
             while (count < langs.Count) {
                 if (langs[count].Contains('S')) {
                     Console.WriteLine(langs[count]);
-                    count++;
                 }
+                count++;
             }
         }
 
         private static void Exercise2(List<string> langs) {
-            Console.WriteLine("---4.1.2---");
+            Console.WriteLine("\n---4.1.2---");
             //LINQを使用する（Where）
             var selected = langs.Where(s => s.Contains('S')); foreach (var lang in selected) { Console.WriteLine(lang); }
         }
 
         private static void Exercise3(List<string> langs) {
-            Console.WriteLine("---4.1.3---");
-            //Swicth式を使用
-
+            Console.WriteLine("\n---4.1.3---");
+            var lang = langs.Find(s => s.Length == 10) ?? "unknown";
+            Console.WriteLine(lang);
         }
     }
 }
