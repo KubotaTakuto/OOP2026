8d148dffb2c34a7dfeabe312622edc1f1b2507d8
久保田 拓斗 <oibc37056@ojs.ac.jp>
2026-06-01 11:19:03 +0900

    問題5.1.4完成

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Exercise01 {
     //5.1.1
+    //プロパティ（P114参照）
     public class YearMonth {
         public int Year { get; init; }
         public int Month { get; init; }
@@ -15,16 +16,20 @@ namespace Exercise01 {
             Month = month;
         }
         //5.1.2
+        //設定されている西暦が21世紀か判定する
         //Yearが2001～2100年ならtrue、それ以外ならfalse
         public bool Is21Century => Year > 2000 && Year <= 2100;
 
         //5.1.3
         public YearMonth AddOneMonth() {
-            if(Month >= 12) {
-                return new YearMonth(Year+1, Month-12);
+            if(Month == 12) {
+                return new YearMonth(Year+1, Month-12+1);
             } else {
                 return new YearMonth(Year, Month + 1);
             }
         }
+
+        //5.1.4
+        public override string ToString() => $"{Year}年{Month}月";  
     }
 }
