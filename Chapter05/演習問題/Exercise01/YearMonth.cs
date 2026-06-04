fcb76fb5d2d90096319f145d8f6a218d208edc89
久保田 拓斗 <oibc37056@ojs.ac.jp>
2026-06-04 09:18:34 +0900

    問題5.2.2完成

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -18,18 +18,18 @@ namespace Exercise01 {
         //5.1.2
         //設定されている西暦が21世紀か判定する
         //Yearが2001～2100年ならtrue、それ以外ならfalse
-        public bool Is21Century => Year > 2000 && Year <= 2100;
+        public bool Is21Century => 2001 <= Year && Year <= 2100;
 
         //5.1.3
         public YearMonth AddOneMonth() {
-            if(Month == 12) {
-                return new YearMonth(Year+1, Month-12+1);
+            if (Month == 12) {
+                return new YearMonth(Year + 1, 1);
             } else {
                 return new YearMonth(Year, Month + 1);
             }
         }
 
         //5.1.4
-        public override string ToString() => $"{Year}年{Month}月";  
+        public override string ToString() => $"{Year}年{Month}月";
     }
 }
---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using Exercise01;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            // 5.2.1
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980, 1),
+                new YearMonth(1990, 4),
+                new YearMonth(2000, 7),
+                new YearMonth(2010, 9),
+                new YearMonth(2024, 12),
+            };
+
+            Console.WriteLine("5.2.2");
+            Exercise2(ymCollection);
+
+            Console.WriteLine("5.2.4");
+            Exercise4(ymCollection);
+
+
+            Console.WriteLine("5.2.5");
+            Exercise5(ymCollection);
+        }
+
+        private static void Exercise2(YearMonth[] ymCollection) {
+            for (int i = 0; i < ymCollection.Length; i++) {
+                Console.WriteLine(ymCollection[i]);
+            }
+        }
+
+        private static void Exercise4(YearMonth[] ymCollection) {
+            foreach (YearMonth ym in ymCollection) {
+                if (ym.Is21Century) {
+                    Console.WriteLine(ym);
+                    break;
+                }
+            }
+        }
+
+        private static void Exercise5(YearMonth[] ymCollection) {
+
+        }
+    }
+}
