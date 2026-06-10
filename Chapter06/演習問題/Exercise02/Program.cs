d88767c9133056db472fcc71263dac7fe02cba59
久保田 拓斗 <oibc37056@ojs.ac.jp>
2026-06-10 10:31:07 +0900

    問題6.2完成

---
+++ Chapter06/演習問題/Exercise02/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("整数文字列：");
+            string stnum =  Console.ReadLine();
+            if (int.TryParse(stnum, out int result)) {
+                var num = result.ToString("#,0");
+                Console.WriteLine(num);
+            } else {
+                Console.WriteLine("整数文字列ではありません");
+            }
+        }
+    }
+}
