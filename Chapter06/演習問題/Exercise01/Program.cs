60647fa70b849c878d26a528334a1e7886c11dbd
久保田 拓斗 <oibc37056@ojs.ac.jp>
2026-06-10 09:59:30 +0900

    問題6.1完成

---
+++ Chapter06/演習問題/Exercise01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("文字列を入力してください(アルファベット)：");
+            var str1 = Console.ReadLine();
+            Console.Write("文字列を入力してください(アルファベット)：");
+            var str2 = Console.ReadLine();
+
+            if (String.Compare(str1, str2, ignoreCase:true) == 0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+}
