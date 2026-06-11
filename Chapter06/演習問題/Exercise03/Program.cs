0f2f7dd5d3ecf1b965febc20432372578d700181
久保田 拓斗 <oibc37056@ojs.ac.jp>
2026-06-11 11:16:07 +0900

    辞書（ディクショナリ）を使った集計　その2

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -80,25 +80,24 @@ namespace Exercise03 {
             var str = text.ToLower().Replace(" ","");
 
             //辞書（ディクショナリ）を使った集計
-            //var alphDicCount = Enumerable.Range('a', 26)
-            //                .ToDictionary(num => ((char)num).ToString(),num => 0);
+            var alphDicCount = Enumerable.Range('a', 26)
+                            .ToDictionary(num => ((char)num), num => 0);
 
-            var dict = new SortedDictionary<char, int>();
+            //var dict = new SortedDictionary<char, int>();
             foreach (var c in str) {
-                //既に店舗名が辞書のキーに登録されているか？
-                if (dict.ContainsKey(c))
-                    dict[c] ++; //登録されている場合
-                else 
-                    dict[c] = 1; //未登録の場合
+                    alphDicCount[c] ++;
             }
-            foreach (var word in dict) {
+
+            foreach (var word in alphDicCount) {
                 Console.WriteLine(word.Key + ":" + word.Value);
             }
 
             //配列を用いた集計
+            var array = Enumerable.Repeat(0, 26).ToArray();
+
+            //'a'から順にカウントして集計
 
 
-            //
         }
     }
 }
