a92eae0ece5e931e13f11d28ff51730c2be57438
久保田 拓斗 <oibc37056@ojs.ac.jp>
2026-06-17 10:35:37 +0900

    問題6.4プログラム例

--- Chapter06/演習問題/Exercise04/Program.cs
+++ Chapter06/演習問題/Exercise04/Program.cs
@@ -5,10 +5,18 @@ namespace Exercise04 {
         static void Main(string[] args) {
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
 
-            var array = line.Split(';','=');
-            for (int i = 0; i < array.Length; i+=2) {
+            var array = line.Split(';', '=');
+            for (int i = 0; i < array.Length; i += 2) {
                 Console.WriteLine($"{ToJapanese(array[i])}：{array[i + 1]}");
             }
+
+            //別解（foreach文）
+            //var pair = line.Split(";");
+            //foreach (var item in pair) {
+            //    var word = item.Split("=");
+            //    Console.WriteLine($"{ToJapanese(word[0])}:{word[1]}");
+            //}
+
         }
         static string ToJapanese(string key) {
             return key switch {
