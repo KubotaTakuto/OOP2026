44f27048508ec543cf7b2e655c2203baba9c17bd
久保田 拓斗 <oibc37056@ojs.ac.jp>
2026-06-29 15:49:43 +0900

    第８章演習問題準備

---
+++ Chapter08/Section02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var lines = File.ReadAllLines("sample.txt");
+            var we = new WordsExtractor(lines);
+            foreach (var word in we.Extract()) {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
