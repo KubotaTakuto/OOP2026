5073b11bd6a52ad04cb87e04c8945d4373a677ed
久保田 拓斗 <oibc37056@ojs.ac.jp>
2026-06-30 14:55:41 +0900

    問題8.1.1完成

--- Chapter08/演習問題/Exercise01/Program.cs
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -10,7 +10,21 @@ namespace Exercise01 {
         }
 
         private static void Exercise1(string text) {
-
+            var dict = new Dictionary<char, int>();
+            var array = text.ToUpper().Order();
+            foreach (var ch in array) {
+                if ('A' <= ch && ch <= 'Z') {
+                    if (dict.ContainsKey(ch)) {
+                        dict[ch]++;
+                    } else {
+                        dict[ch] = 1;
+                    }
+                }
+            }
+            
+            foreach (var alph in dict) {
+                Console.WriteLine($"{alph.Key}:{alph.Value}");
+            }
         }
 
         private static void Exercise2(string text) {
